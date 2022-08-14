986cacbbca5b00fcb2074b038ee401dc8f0d9ed8
Christopher Coverdale <chris.coverdale24@gmail.com>
2022-08-14 12:23:48 +0100

    add test for Execute() overload accepting a void function

--- threadpool/tests/test_thread_pool.cpp
+++ threadpool/tests/test_thread_pool.cpp
@@ -3,6 +3,8 @@
 #include "threadpool/thread_pool.h"
 
 
+// Adding multiple tests here since not sure if creating threads per test might
+// lead to irregularities in results.
 TEST(ThreadPool, Init) {
     constexpr int num_threads{5}; 
 
@@ -14,4 +16,18 @@ TEST(ThreadPool, Init) {
     future1.wait();
 
     ASSERT_EQ(future1.get(), 3);
+
+    // This is a test to that allows the caller to provide their own future
+    // instead of receiving a future from the threadpool.
+    // lead to irregularities in results.
+    auto task_ptr = std::make_shared<std::packaged_task<int()>>(std::bind(add, 5, 5));
+    std::function<void()> wrapper_fn = [task_ptr]() {
+      (*task_ptr)();
+    };
+    auto future2 = task_ptr->get_future();
+
+    thread_pool.Execute(wrapper_fn);
+
+    future2.wait();
+    ASSERT_EQ(future2.get(), 10);
 }
