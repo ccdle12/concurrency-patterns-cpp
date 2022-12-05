69555c9ba94cd0e88666e9b1c05fdc3b055d0936
Christopher Coverdale <chris.coverdale24@gmail.com>
2022-12-05 19:00:22 +0000

    wal: separate implementation into .h and .cpp files

--- write-ahead-log/include/wal/wal.h
+++ write-ahead-log/include/wal/wal.h
@@ -54,45 +54,11 @@ class WriteAheadLog
   private:
     // TODO: Add a mutex
     std::fstream m_log_file;
-
-    inline void assert_file_open() const
-    {
-        if (!m_log_file.is_open())
-            throw std::runtime_error("Failed to open log file");
-    }
+    void assert_file_open() const;
 
   public:
-    WriteAheadLog(const std::string& file)
-    {
-        m_log_file.open(file, std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
-        assert_file_open();
-    }
-
-    void Write(const Entry& entry)
-    { 
-        assert_file_open();
-        m_log_file << entry;
-    }
-
-    std::vector<Entry> Read()
-    {
-        assert_file_open();
-
-        m_log_file.seekg(0, std::ios::end);
-        auto end = m_log_file.tellg();
-
-        m_log_file.seekg(0);
-
-        std::vector<Entry> entries;
-        for (int i = 0; i < end; i = m_log_file.tellg())
-        {
-            Entry entry;
-            m_log_file >> entry;
-
-            entries.push_back(entry);
-        }
-
-        return entries;
-    }
+    WriteAheadLog(const std::string& file);
+    void Write(const Entry& entry);
+    std::vector<Entry> Read();
   };
-};
+}
