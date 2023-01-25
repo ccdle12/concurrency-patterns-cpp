5f40db1aebe3762d34d6eb1bddec85e36ac19921
Christopher Coverdale <chris.coverdale24@gmail.com>
2023-01-25 19:27:54 +0000

    wal: add basic segmented log

--- write-ahead-log/include/wal/wal.h
+++ write-ahead-log/include/wal/wal.h
@@ -59,8 +59,12 @@ class Entry
 class IWAL
 {
 public:
+    // Writes an entry to the log.
     virtual void Write(const Entry& entry) = 0;
+
+    // Reads all entries from the log.
     virtual std::vector<Entry> Read() = 0;
+
     virtual ~IWAL() = default;
 };
 
@@ -68,13 +72,34 @@ class WriteAheadLog : public IWAL
 {
   private:
     // TODO: Add a mutex
+
+    // The path to the log folder.
+    std::string m_folder_path;
+
+    // The current log file being used for the current segment.
     std::fstream m_log_file;
+
+    // Method that throws if the the current log file is not open.
     void assert_file_open() const;
+
+    // The current segment being used.
+    std::vector<Entry> m_current_segment;
+
+    // The max size of each segment.
+    std::size_t m_max_segment_size;
+
+    // The counter used to get the highest segment number and used in the logfile.
+    std::size_t m_segment_counter;
+
+    // Called by the constructor to create the log directory.
+    void SetupLogDir(const std::string& folder_path);
 
   public:
     WriteAheadLog() = default;
     ~WriteAheadLog() override = default;
+
     WriteAheadLog(const std::string& file);
+    WriteAheadLog(const std::string& file, std::size_t max_segment_size);
 
     void Write(const Entry& entry) override;
     std::vector<Entry> Read() override;
--- write-ahead-log/src/wal.cpp
+++ write-ahead-log/src/wal.cpp
@@ -1,5 +1,8 @@
 #include "wal/wal.h"
 #include <filesystem>
+#include <ctype.h>
+#include <bits/stdc++.h>
+#include <iterator>
 
 using namespace wal;
 namespace fs = std::filesystem;
@@ -10,35 +13,105 @@ void WriteAheadLog::assert_file_open() const
         throw std::runtime_error("Failed to open log file");
 }
 
-WriteAheadLog::WriteAheadLog(const std::string& file)
+WriteAheadLog::WriteAheadLog(const std::string& folder_path) :
+    m_folder_path{folder_path}, m_max_segment_size{3}
 {
-    m_log_file.open(file, std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
-    assert_file_open();
+    SetupLogDir(folder_path);
+}
+
+WriteAheadLog::WriteAheadLog(const std::string& folder_path, std::size_t max_segment_size) :
+    m_folder_path{folder_path}, m_max_segment_size{max_segment_size}
+{
+    SetupLogDir(folder_path);
+}
+
+void WriteAheadLog::SetupLogDir(const std::string& folder_path)
+{
+    // Doesn't override the existing directory if it exists.
+    fs::create_directories(folder_path);
+
+    // Find the most recent log file and open it.
+    auto fs_iter = fs::recursive_directory_iterator(folder_path);
+    auto highest_log = 0;
+    for (auto const& log : fs_iter) {
+        auto log_prefix = log.path().filename().string()[0];
+
+        if (std::isdigit(log_prefix)) {
+            int log_num = log_prefix - '0';
+            if (log_num > highest_log)
+                highest_log = log_num;
+        }
+    }
+
+    std::stringstream log_file;
+    log_file << folder_path
+             << "/"
+             << highest_log
+             << "_log.dat";
+
+    m_log_file.open(log_file.str(), std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
 }
 
 void WriteAheadLog::Write(const Entry& entry)
 { 
     assert_file_open();
-    m_log_file << entry;
+
+    // This changes the previously assumed contract of only writing to the disk
+    // once the segment is full.
+    m_current_segment.push_back(std::move(entry));
+
+    if (m_current_segment.size() >= m_max_segment_size)
+    {
+        for (auto const& entry : m_current_segment)
+            m_log_file << entry;
+
+        m_current_segment.clear();
+        m_log_file.close();
+
+        m_segment_counter += 1;
+
+        std::stringstream new_segment_file;
+        new_segment_file << m_folder_path
+                         << "/"
+                         << m_segment_counter
+                         << "_log.dat";
+
+        m_log_file.open(new_segment_file.str(), std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
+    }
+
 }
 
 std::vector<Entry> WriteAheadLog::Read() 
 {
-    assert_file_open();
-
-    m_log_file.seekg(0, std::ios::end);
-    auto end = m_log_file.tellg();
-
-    m_log_file.seekg(0);
+    auto fs_iter = fs::recursive_directory_iterator(m_folder_path);
 
     std::vector<Entry> entries;
-    for (int i = 0; i < end; i = m_log_file.tellg())
-    {
-        Entry entry;
-        m_log_file >> entry;
+    for (auto const& log : fs_iter) {
+        std::fstream log_file;
+        log_file.open(log.path().string(), std::ios::binary | std::ios::in);
 
-        entries.push_back(std::move(entry));
+        log_file.seekg(0, std::ios::end);
+        auto end = log_file.tellg();
+
+        log_file.seekg(0);
+
+        // TODO: This is horrible, but builds the vector of entries
+        // in the correct historical order.
+        std::vector<Entry> tmp_buf;
+        for (int i = 0; i < end; i = log_file.tellg())
+        {
+            Entry entry;
+            log_file >> entry;
+
+            tmp_buf.push_back(std::move(entry));
+        }
+
+        std::reverse(tmp_buf.begin(), tmp_buf.end());
+        for (auto& e : tmp_buf) {
+            entries.push_back(std::move(e));
+        }
     }
 
+    std::reverse(entries.begin(), entries.end());
     return entries;
 }
